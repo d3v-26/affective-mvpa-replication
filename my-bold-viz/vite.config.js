a001b43e0bf39b69e3555d046299a7f36129ed1f
Dev Patel <68773259+d3v-26@users.noreply.github.com>
2026-02-19 14:30:25 -0500

    update config

--- my-bold-viz/vite.config.js
+++ my-bold-viz/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_ACTIONS ? '/affective-mvpa-retinotopic/' : '/',
+  base: process.env.GITHUB_ACTIONS ? '/affective-mvpa-replication/' : '/',
 })
